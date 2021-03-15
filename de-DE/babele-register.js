5aaa8bf47ecee125b96d1da8354f1b5d6d3afed8
xmichix <69576798+xmichix@users.noreply.github.com>
2021-03-15 23:01:54 +0100

    mh

--- de-DE/babele-register.js
+++ de-DE/babele-register.js
@@ -3,7 +3,7 @@ Hooks.once('init', () => {
 
 	if(typeof Babele !== 'undefined') {
 		Babele.get().register({
-			module: 'dnd35e-de-DE',
+			module: 'FoundryVTT-dnd35e-de',
 			lang: 'de',
 			dir: 'compendium'
 		});
